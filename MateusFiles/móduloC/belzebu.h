0eeb6809ce1846a2ee4330b2a0853f1c22150fc7
hpfred <hpfred@hpfred-VirtualBox.(none)>
2021-04-11 21:38:11 -0300

    Added a few things to make the link between C and Fortran work, to no avail

---
+++ MateusFiles/móduloC/belzebu.h
@@ -0,0 +1,5 @@
+int CheckTextOpen(FILE *TextFile);
+int GetCharacterQuantityF(FILE *TextFile);
+char *GetTextFromFile(FILE *TextFile, char *Text, int CharacterQuantity);
+char *GetWordFromFile(FILE *TextFile, char *Word, int CharacterQuantity);
+void PrintText(char *Text, int TextSize);
