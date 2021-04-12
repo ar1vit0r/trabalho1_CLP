cca418e1d7b647b9af110155a67074d8d493d8a6
hpfred <hpfred@hpfred-VirtualBox.(none)>
2021-04-11 21:49:08 -0300

    Removed some stuff

--- móduloC/belzebu.h
+++
@@ -1,5 +0,0 @@
-int CheckTextOpen(FILE *TextFile);
-int GetCharacterQuantityF(FILE *TextFile);
-char *GetTextFromFile(FILE *TextFile, char *Text, int CharacterQuantity);
-char *GetWordFromFile(FILE *TextFile, char *Word, int CharacterQuantity);
-void PrintText(char *Text, int TextSize);
